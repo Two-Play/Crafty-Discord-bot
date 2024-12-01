50e24ac08deeef62f481805b086622e7ee4797d1
Philippe <twoplay19@gmail.com>
2024-12-01 19:24:33 +0100

    fix version

--- core/constants.py
+++ core/constants.py
@@ -8,3 +8,4 @@ STATUS_SUCCESS = 200
 API_ENDPOINT = '/api/v2/servers/'
 GUILD_ID: int = int(os.getenv('GUILD_ID', 0))
 AUTO_STOP_SLEEP_TIME: int = int(os.getenv('AUTO_STOP_SLEEP_TIME', 1800))
+CRAFTY_BOT_VERSION='0.2.0'
